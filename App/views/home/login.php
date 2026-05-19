505d30aafa84120951b422307a5533b7500b295e
DaoSonDong <sondonghy6@gmail.com>
2026-05-19 23:38:05 +0700

    add login feature + middleware

--- App/views/home/login.php
+++ App/views/home/login.php
@@ -10,8 +10,10 @@
     <form action="/auth/login" method="POST">
         <label for="username">Tên đăng nhập</label>
         <input type="text" id="username" name="username" required>
+        <br><br>
         <label for="password">Mật khẩu</label>
         <input type="password" id="password" name="password" required>
+        <br><br>
         <input type="submit" value="Đăng nhập">
     </form>
 </body>
--- Public/index.php
+++ Public/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../app/middleware.php";
+require_once "../App/middleware.php";
 $middelware = new middleware();
 $middelware->checklogin();
 $app = new App();
