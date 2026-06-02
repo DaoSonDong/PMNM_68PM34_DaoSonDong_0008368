7ff691ef6b03352ff51160d6a82948ab15f1ae13
DaoSonDong <sondonghy6@gmail.com>
2026-06-02 22:03:06 +0700

    feat<sinhvien>: create Sinhvien

--- App/controllers/sinhvien.php
+++ App/controllers/sinhvien.php
@@ -8,12 +8,31 @@ class sinhvien extends Controller
     $sinhvien = $SinhvienModel->getAllSinhvien();
     // // Trả về view
     // require_once '../App/views/sinhvien/index.php';
-    $this->view('sinhvien/index', ['sinhvien' => $sinhvien, 'title' => 'Danh sách sinh viên']);
+    $this->view("layout/masterlayout", ['viewname'=>'sinhvien/index', 'sinhvien'=>$sinhvien]);
   }
 
   public function create()
   {
     require_once '../App/views/sinhvien/create.php';
+  }
+
+  public function store(){
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      $data = [
+        'mssv' => $_POST['mssv']
+        'hoten' => $_POST['hoten'],
+        'gioitinh' => $_POST['gioitinh'],
+      ];
+      $sinhvienModel = $this->model('sinhvienModel');
+      $result = $sinhvienModel->create($data);
+      if ($result) {
+        $_SESSION['success'] = "Thêm sinh viên thành công!";
+        header("Location: /sinhvien/index");
+        exit();
+      } else {
+        $_SESSION['error'] = "Thêm sinh viên thất bại!";
+      }
+    }
   }
 }
 ?>
